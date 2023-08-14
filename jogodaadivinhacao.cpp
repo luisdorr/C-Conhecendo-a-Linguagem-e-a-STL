990aae4ff67548a5d512292c4ea98d5b66edced9
Luis Dorr <Luisdorr@outlook.com>
2023-08-13 23:05:07 -0300

    create a file just to test the outputs in c++

---
+++ jogodaadivinhacao.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+
+using namespace std;
+
+int main () {
+    cout << "*************************************" << endl;
+    cout << "* Bem-vindos ao jogo da Adivinhacao *" << endl;
+    cout << "*************************************" << endl;
+}
