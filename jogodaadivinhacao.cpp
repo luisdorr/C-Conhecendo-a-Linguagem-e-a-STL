95ad0b46acf6412c0c1cc4c3c3667d40b7abea0c
Luis Dorr <Luisdorr@outlook.com>
2023-08-14 18:13:49 -0300

    use variables and if/else clauses in C++

--- jogodaadivinhacao.cpp
+++ jogodaadivinhacao.cpp
@@ -1,9 +1,30 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
-int main () {
+int main() {
     cout << "*************************************" << endl;
-    cout << "* Bem-vindos ao jogo da Adivinhacao *" << endl;
+    cout << "* Welcome to the Guessing Game! *" << endl;
     cout << "*************************************" << endl;
+
+    const int SECRET_NUMBER = 32;
+
+    int guess;
+    cout << "What's your guess? ";
+    cin >> guess;
+
+    cout << "Your guess value is: " << guess << endl;
+
+    bool correct = guess == SECRET_NUMBER;
+
+    if (correct) {
+        cout << "Congratulations, you guessed the secret number!" << endl;
+    } else {
+        cout << "You missed! ";
+        if (guess > SECRET_NUMBER) {
+            cout << "Your guess was higher than the secret number!";
+        } else if (guess < SECRET_NUMBER) {
+            cout << "Your guess was lower than the secret number!";
+        }
+    }
 }
